7b5abd8e4738cff68e4b2de19cc10848e6e9eef8
pourzanj <arya@umail.ucsb.edu>
2017-03-15 07:29:45 -0700

    updated columns selected to include columns that are important for measuring how much blood a patient needs e.g. lactate

--- ImportExploratory.R
+++ ImportExploratory.R
@@ -1,22 +1,26 @@
 library(dplyr)
 library(lubridate)
+library(readr)
 
 #split data in to a general tbl and a tbl containing all factors and other blood tests
-coag <- read.csv("Data/ACITdataset_UCSB_23Feb15.csv") %>%
-  tbl_df %>%
+coag <- read_csv("Data/ACITdataset_UCSB_23Feb15.csv") %>%
   select(acitnum, male, age, bmi,
          mechtype, blunt, tbi, edarrivalgcs, numribfxs, admitday_intubated,
-         aishead1, aisface2, aischest3, aisabdomen4, aisextremity5,
+         iss, aishead1, aisface2, aischest3, aisabdomen4, aisextremity5,
          
          prehosp_crystalloids, icu_0to6h_blood_units,
          
          injurydatetime, edarrivaldatetime, hr0_datetime, datetimeofdeath,
          
-         hr0_temp, hr0_hr, hr0_sbp, hr0_basedefexc, hr0_inr,
+         hr0_temp, hr0_hr, hr0_resprate, hr0_sbp,
          
-         hr0_factorii, hr0_factorv, hr0_factorvii, hr0_factorviii,
-         hr0_factorix, hr0_factorx, hr0_ddimer, hr0_apc, hr0_pc,
-         hr0_atiii, hr0_fibrinogen_rlab) %>%
+         hr0_ph, hr0_paco2, hr0_pao2, hr0_hco3, hr0_serumco2, hr0_bun, hr0_hgb,
+         hr0_lactate, hr0_basedefexc, hr0_inr
+         
+         #hr0_factorii, hr0_factorv, hr0_factorvii, hr0_factorviii,
+         #hr0_factorix, hr0_factorx, hr0_ddimer, hr0_apc, hr0_pc,
+         #hr0_atiii, hr0_fibrinogen_rlab
+         ) %>%
   
   #clean some of the columns
   mutate(mechtype = as.character(mechtype)) %>%
@@ -26,6 +30,10 @@ coag <- read.csv("Data/ACITdataset_UCSB_23Feb15.csv") %>%
   mutate(blunt = as.character(blunt)) %>%
   mutate(blunt = ifelse(blunt == "", as.character(NA), blunt)) %>%
   mutate(blunt = as.factor(blunt)) %>%
+  
+  mutate(hr0_lactate = ifelse(hr0_lactate == "", as.character(NA), hr0_lactate)) %>%
+  mutate(hr0_lactate = ifelse(hr0_lactate == ">15.0", NA, hr0_lactate)) %>%
+  mutate(hr0_lactate = as.numeric(hr0_lactate)) %>%
   
   #wrangle times
   mutate_each(funs(parse_date_time(., "m/d/y H:M")),
